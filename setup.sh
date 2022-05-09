fbc6da77728eab96d01f58452af1ed3ef69b500b
bhawiyuga <bhawiyuga@ub.ac.id>
2022-05-10 06:32:53 +0700

    initial

---
+++ setup.sh
@@ -0,0 +1,45 @@
+
+ARCH=$(arch)
+
+if [ "$ARCH" = "arm64" ]; then
+
+echo "Setup for arm64 device."
+
+rm -rf fabric
+rm -rf fabric-samples
+rm -rf fabric-ca
+
+git clone -b release-2.2 https://github.com/hyperledger/fabric.git
+git clone https://github.com/hyperledger/fabric-ca.git
+git clone -b release-2.2 https://github.com/hyperledger/fabric-samples.git
+
+rm -rf fabric/images/*
+cp -R images-fabric/* fabric/images
+
+rm -rf fabric-ca/images/*
+cp -R images-fabric-ca/* fabric-ca/images
+
+cd fabric/
+make docker-clean
+make docker
+make native
+# sudo cp build/bin/* /usr/local/bin/
+export PATH=$(pwd)/bin:$PATH
+cd ..
+
+cd fabric-ca/
+make docker-clean
+make docker
+cd ..
+
+elif [ "$ARCH" = "amd64" ]; then
+
+echo "Setup for amd64 device."
+
+curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.2.5 1.5.2
+
+else
+
+echo "Unsupported architecture."
+
+fi
